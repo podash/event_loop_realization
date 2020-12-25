2089f8cc9b22f1052ed55b3deec13256fb3f2674
MaryLynJuana <mary_juana@ukr.net>
2020-12-25 14:22:14 +0200

    fix(core): Make command types exported

--- commands/add.go
+++ commands/add.go
@@ -6,19 +6,19 @@ import (
 	"github.com/Trafle/event_loop_realization/engine"
 )
 
-type addCommand struct {
+type AddCommand struct {
 	Arg1, Arg2 int
 }
 
-type printCommand struct {
+type PrintCommand struct {
 	Arg string
 }
 
-func (a *addCommand) Execute(loop engine.Handler) {
+func (a *AddCommand) Execute(loop engine.Handler) {
 	sum := a.Arg1 + a.Arg2
-	loop.Post(&printCommand{Arg: strconv.Itoa(sum)})
+	loop.Post(&PrintCommand{Arg: strconv.Itoa(sum)})
 }
 
-func (p *printCommand) Execute(loop engine.Handler) {
+func (p *PrintCommand) Execute(loop engine.Handler) {
 	fmt.Println(p.Arg)
 }
